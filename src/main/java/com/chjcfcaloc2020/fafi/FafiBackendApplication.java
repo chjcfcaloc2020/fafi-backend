98d457eec5e5b4a20fd17133e98710619701004f
chjcfcaloc2020 <nguyentruongchi83@gmail.com>
2025-08-28 21:49:18 +0700

    feat: add base project

---
+++ src/main/java/com/chjcfcaloc2020/fafi/FafiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chjcfcaloc2020.fafi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FafiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FafiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chjcfcaloc2020/fafi/FafiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chjcfcaloc2020.fafi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FafiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
